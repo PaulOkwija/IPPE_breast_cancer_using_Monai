3e288d80ac9291f6b2f2fb7407593501f01f4319
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-12-02 15:46:29 +0300

    Update train.py

--- train.py
+++ train.py
@@ -16,6 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 import os
 import shutil
 import tempfile
@@ -66,3 +67,53 @@ val_transforms = Compose(
 
 y_pred_trans = Compose([Activations(softmax=True)])
 y_trans = Compose([AsDiscrete(to_onehot=num_class)])
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path", "-a", dest="path", help="ip address of probe.", required=True)
+    parser.add_argument("--mask", "-p", dest="masks", default=False, help="Are there masks in the folders", required=True)
+    parser.add_argument("--show_samples","-ss", dest="samples", default=False, help="image output width in pixels")
+    parser.add_argument("--height", "-ht", dest="height", type=int, help="image output height in pixels")
+
+    args = parser.parse_args()
+    mask_present = args.masks
+    path = args.path
+
+    image_files_list = []
+    image_class = []
+
+    data_files, num_class, class_names = path_list(path,mask_present)
+    num_each = [len(data_files[i]) for i in range(num_class)]
+
+    for i in range(num_class):
+        image_files_list.extend(data_files[i])
+        image_class.extend([i] * num_each[i])
+        
+    num_total = len(image_class)
+    image_width, image_height = PIL.Image.open(image_files_list[0]).size
+
+    print(f"Total image count: {num_total}")
+    print(f"Image dimensions: {image_width} x {image_height}")
+    print(f"Label names: {class_names}")
+    print(f"Label counts: {num_each}")
+
+    train_transforms = Compose(
+        [
+            LoadImage(image_only=True),
+            EnsureChannelFirst(),
+            ScaleIntensity(),
+            RandRotate(range_x=np.pi / 12, prob=0.5, keep_size=True),
+            RandFlip(spatial_axis=0, prob=0.5),
+            RandZoom(min_zoom=0.9, max_zoom=1.1, prob=0.5),
+        ]
+    )
+
+    val_transforms = Compose(
+        [LoadImage(image_only=True), EnsureChannelFirst(), ScaleIntensity()])
+
+    y_pred_trans = Compose([Activations(softmax=True)])
+    y_trans = Compose([AsDiscrete(to_onehot=num_class)])
+
+if __name__ == "__main__":
+    main()
